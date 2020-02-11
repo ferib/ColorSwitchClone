45adbd3f9f7b7e430e6fbbebe979b31c7f2b03a7
ferib <lopke123@live.nl>
2020-02-11 20:05:39 +0100

    basic circle and player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System;
+
+public class Player : MonoBehaviour
+{
+
+    public float JumpForce = 10.0f;
+    public Rigidbody2D rb;
+    public SpriteRenderer sr;
+    public Color colorCyan;
+    public Color colorYellow;
+    public Color colorPink;
+    public Color colorMagenta;
+
+    private ObjColor PlayerColor;
+    private Color[] colorArray;
+
+    void Start()
+    {
+        colorArray = new Color[] { colorCyan, colorYellow, colorPink, colorMagenta };
+        SetRandomColor();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = Vector2.up * JumpForce;
+        }
+    }
+
+    void OnTriggerEnter2D (Collider2D col)
+    {
+        if (col.tag == PlayerColor.ToString())
+        {
+            SetRandomColor();
+            Debug.Log("nailed it");
+        }
+        else
+            Debug.Log("ded");
+    }
+
+    void SetRandomColor()
+    {
+        ObjColor newColor = ObjColor.None; 
+        while(newColor == ObjColor.None || newColor == PlayerColor) //its not exactly "random" since color should always change (noticable change ;) )
+            newColor = (ObjColor)UnityEngine.Random.Range(0, 3); 
+
+        PlayerColor = newColor;
+        sr.color = colorArray[(int)PlayerColor];
+        Debug.Log("Color set to " + colorArray[(int)PlayerColor].ToString());
+    }
+
+    //i prefer this :p
+    enum ObjColor
+    {
+        Cyan = 0,
+        Yellow = 1,
+        Pink = 2,
+        Magenta = 3,
+        None = 4
+    }
+}
---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+
+    public float speed = 100.0f;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        transform.Rotate(0f, 0f, speed * Time.deltaTime);
+    }
+}
